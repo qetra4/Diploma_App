712f8c88e0ef6f4c885c033c5cc199d668b2eb4d
ioanna <andofilopino@inbox.ru>
2025-02-23 18:55:03 +0300

    Pies +

--- data/views.py
+++ data/views.py
@@ -90,7 +90,6 @@ def user_data(request, user_id):
 
 @login_required
 def upload_csv(request):
-    print("the func upload is used")
     if request.method == 'POST':
         form = CSVUploadForm(request.POST, request.FILES)
         if form.is_valid():
@@ -114,40 +113,32 @@ def upload_csv(request):
 
 def handle_uploaded_file(file, model, request):
     if file:
-        print("File is present")  # Для отладки
         decoded_file = file.read().decode('utf-8').splitlines()
-        print("Decoded file:", decoded_file)  # Для отладки
+        print("Decoded file:", decoded_file)
         reader = csv.DictReader(decoded_file)
         for row in reader:
             try:
-                print("Processing row:", row)  # Для отладки
-                # Преобразуем value в float
                 row['value'] = float(row['value'])
                 model.objects.create(
                     id=row['id'],
                     user=request.user,
-                    value=row['value'],  # Теперь это число, а не строка
+                    value=row['value'],
                     time=datetime.strptime(row['time'], '%Y-%m-%d %H:%M')
                 )
-                print("Saved data:", row)  # Для отладки
             except Exception as e:
-                print("Error saving data:", e)  # Для отладки
-    else:
-        print("No file uploaded")  # Для отладки
+                print("Error saving data:", e)
 
 def delete_data(request):
     if request.method == 'POST':
-        # Удаляем все данные из всех моделей
         Pulse.objects.all().delete()
         Steps.objects.all().delete()
         Distance.objects.all().delete()
         Calories.objects.all().delete()
         
         messages.success(request, 'Все данные успешно удалены.')
-    return redirect('user_data', user_id=request.user.id)  # Перенаправляем на главную страницу или другую страницу       
+    return redirect('user_data', user_id=request.user.id)     
     
 
-# Функция для создания гистограммы
 def create_histogram(data, title, xlabel, ylabel):
     plt.figure(figsize=(8, 6))
     plt.bar(range(len(data)), list(data.values()), color='#c270db')
@@ -156,22 +147,46 @@ def create_histogram(data, title, xlabel, ylabel):
     plt.ylabel(ylabel, fontsize=14)
     plt.xticks(range(len(data)), list(data.keys()), rotation=45)
     plt.tight_layout()
-
-    # Сохраняем график в буфер
     buf = io.BytesIO()
     plt.savefig(buf, format='png')
     plt.close()
     buf.seek(0)
     return base64.b64encode(buf.read()).decode('utf-8')
 
-# Функция для создания круговой диаграммы
+import io
+import base64
+import matplotlib.pyplot as plt
+
 def create_pie_chart(data, title):
     plt.figure(figsize=(8, 6))
-    plt.pie(data.values(), labels=data.keys(), autopct='%1.1f%%', colors=['#c270db', '#9947b2', '#b73fd5'])
+
+    colors = [ '#ce83e4', '#943796', '#efffaa']
+
+    labels = []
+    sizes = []
+    colors_filtered = []
+    for label, size in data.items():
+        if size > 0:
+            labels.append(label)
+            sizes.append(size)
+            colors_filtered.append(colors[list(data.keys()).index(label)])
+
+    wedges, texts, autotexts = plt.pie(
+        sizes,
+        labels=labels,
+        autopct='%1.1f%%',
+        colors=colors_filtered,
+        startangle=90, 
+        labeldistance=1.1,
+        pctdistance=0.8,
+    )
+    for text in texts:
+        text.set_fontsize(10)
+    for autotext in autotexts:
+        autotext.set_fontsize(10)
+
     plt.title(title, fontsize=16)
     plt.tight_layout()
-
-    # Сохраняем график в буфер
     buf = io.BytesIO()
     plt.savefig(buf, format='png')
     plt.close()
@@ -295,27 +310,102 @@ def distance_histogram(request):
     return render(request, 'data/chart.html', {'chart': chart, 'title': 'Гистограмма дистанции по дням'})
 
 
-# View для круговой диаграммы пульса
 def pulse_pie_chart(request):
-    data = {'Низкий': 30, 'Средний': 50, 'Высокий': 20}
-    chart = create_pie_chart(data, 'Круговая диаграмма пульса')
-    return render(request, 'data/chart.html', {'chart': chart})
+    user = request.user
+    filter_type = request.GET.get('filter', 'all')
+    now = timezone.now()
+    if filter_type == 'week':
+        start_date = now - timedelta(days=7)
+    elif filter_type == 'month':
+        start_date = now - timedelta(days=30)
+    else:
+        start_date = None
+    pulses = Pulse.objects.filter(user=user)
+    if start_date:
+        pulses = pulses.filter(time__gte=start_date)
+    data = {'Низкий': 0, 'Средний': 0, 'Высокий': 0}
+    for pulse in pulses:
+        if pulse.value < 65:
+            data['Низкий'] += 1
+        elif 65 <= pulse.value <= 100:
+            data['Средний'] += 1
+        else:
+            data['Высокий'] += 1
+    chart = create_pie_chart(data, 'Круговая диаграмма количество дней по пульсу')
+    return render(request, 'data/chart.html', {'chart': chart, 'title': 'Круговая диаграмма пульса'})
 
-# View для круговой диаграммы шагов
+
 def steps_pie_chart(request):
-    data = {'Низкий': 40, 'Средний': 40, 'Высокий': 20}
-    chart = create_pie_chart(data, 'Круговая диаграмма шагов')
-    return render(request, 'data/chart.html', {'chart': chart})
+    user = request.user
+    filter_type = request.GET.get('filter', 'all')
+    now = timezone.now()
+    if filter_type == 'week':
+        start_date = now - timedelta(days=7)
+    elif filter_type == 'month':
+        start_date = now - timedelta(days=30)
+    else:
+        start_date = None
+    steps = Steps.objects.filter(user=user)
+    if start_date:
+        steps = steps.filter(time__gte=start_date)
+    data = {'Мало шагов': 0, 'Среднее количество шагов': 0, 'Много шагов': 0}
+    for step in steps:
+        if step.value < 5000:
+            data['Мало шагов'] += 1
+        elif 5500 <= step.value <= 6000:
+            data['Среднее количество шагов'] += 1
+        else:
+            data['Много шагов'] += 1
+    chart = create_pie_chart(data, 'Круговая диаграмма количества дней по пройденным шагам')
+    return render(request, 'data/chart.html', {'chart': chart, 'title': 'Круговая диаграмма шагов'})
 
-# View для круговой диаграммы пульса
+
 def calories_pie_chart(request):
-    data = {'Низкий': 30, 'Средний': 50, 'Высокий': 20}
-    chart = create_pie_chart(data, 'Круговая диаграмма пульса')
-    return render(request, 'data/chart.html', {'chart': chart})
+    user = request.user
+    filter_type = request.GET.get('filter', 'all')
+    now = timezone.now()
+    if filter_type == 'week':
+        start_date = now - timedelta(days=7)
+    elif filter_type == 'month':
+        start_date = now - timedelta(days=30)
+    else:
+        start_date = None
+    calories = Calories.objects.filter(user=user)
+    if start_date:
+        calories = calories.filter(time__gte=start_date)
+    data = {'Мало калорий': 0, 'Среднее количество калорий': 0, 'Много калорий': 0}
+    for calorie in calories:
+        if calorie.value < 290:
+            data['Мало калорий'] += 1
+        elif 290 <= calorie.value <= 300:
+            data['Среднее количество калорий'] += 1
+        else:
+            data['Много калорий'] += 1
+    chart = create_pie_chart(data, 'Круговая диаграмма дней по сожженных калорий')
+    return render(request, 'data/chart.html', {'chart': chart, 'title': 'Круговая диаграмма калорий'})
 
-# View для круговой диаграммы шагов
+
 def distance_pie_chart(request):
-    data = {'Низкий': 40, 'Средний': 40, 'Высокий': 20}
-    chart = create_pie_chart(data, 'Круговая диаграмма шагов')
-    return render(request, 'data/chart.html', {'chart': chart})
+    user = request.user
+    filter_type = request.GET.get('filter', 'all')
+    now = timezone.now()
+    if filter_type == 'week':
+        start_date = now - timedelta(days=7)
+    elif filter_type == 'month':
+        start_date = now - timedelta(days=30)
+    else:
+        start_date = None
+    distance = Distance.objects.filter(user=user)
+    if start_date:
+        distance = distance.filter(time__gte=start_date)
+    data = {'Малое расстояние': 0, 'Среднее расстояние': 0, 'Большое расстояние': 0}
+    for dist in distance:
+        if dist.value < 5000:
+            data['Малое расстояние'] += 1
+        elif 5000 <= dist.value <= 5200:
+            data['Среднее расстояние'] += 1
+        else:
+            data['Большое расстояние'] += 1
+    chart = create_pie_chart(data, 'Круговая диаграмма дней по пройденной дистанции в метрах')
+    return render(request, 'data/chart.html', {'chart': chart, 'title': 'Круговая диаграмма дистанции'})
 
